324846c73a3eab85b5c5ac5744dafb2a8539bf8d
Naman Nehra <naman.nehra98@gmail.com>
2022-05-19 20:33:35 +0530

    Fix demo URL

--- vite.demo.ts
+++ vite.demo.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
-    base: '/v3/',
+    base: '/flipping-pages/v3/',
     build: {
         target: 'es2021',
         outDir: 'dist-demo/v3',
